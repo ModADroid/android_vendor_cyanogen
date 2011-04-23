cdf13e0b8e81579bfa4a394915284300628d979b
jt1134 <jt1134@gmail.com>
2011-04-22 23:14:56 -0500

    fascinate i500 additions

---
+++ products/cyanogen_fascinate.mk
@@ -0,0 +1,40 @@
+# Inherit AOSP device configuration for fascinate.
+$(call inherit-product, device/samsung/fascinate/full_fascinate.mk)
+
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/cyanogen/products/common_full.mk)
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := cyanogen_fascinate
+PRODUCT_BRAND := verizon
+PRODUCT_DEVICE := fascinate
+PRODUCT_MODEL := SCH-I500
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=soju BUILD_ID=GRI40 BUILD_DISPLAY_ID=GRI40 BUILD_FINGERPRINT=google/soju/crespo:2.3.3/GRI40/102588:user/release-keys PRIVATE_BUILD_DESC="soju-user 2.3.3 GRI40 102588 release-keys"
+
+# Extra Fascinate overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/fascinate
+
+#
+# Set ro.modversion
+#
+ifdef CYANOGEN_NIGHTLY
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.modversion=CyanogenMod-7-$(shell date +%m%d%Y)-NIGHTLY-Fascinate
+else
+    ifdef CYANOGEN_RELEASE
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=CyanogenMod-7.0.0-Fascinate
+    else
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=CyanogenMod-7.0.0-Fascinate-KANG
+    endif
+endif
+
+#
+# Copy fascinate specific prebuilt files
+#
+PRODUCT_COPY_FILES +=  \
+    vendor/cyanogen/prebuilt/hdpi/media/bootanimation.zip:system/media/bootanimation.zip
